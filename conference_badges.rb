574d7b4975f0538cb4fe455e2b3fd05c6b848ba9
AJ Guiles <ashiguiles@gmail.com>
2019-02-28 04:20:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,30 +1,30 @@
 
-speakers=["Edsger", "Ada", "Charles", "Alan", "Grace", 
+# speakers=["Edsger", "Ada", "Charles", "Alan", "Grace", 
 
-Def badge_maker(name)
-puts "Hello, my name is #{name}."
-end 
+# Def badge_maker(name)
+# puts "Hello, my name is #{name}."
+# end 
 
 
-def batch_badge_creator(names)
-  names.collect { |name| puts badge_maker(name) }
-### for each speaker in array implemanet badge_maker
-end
-end
+# def batch_badge_creator(names)
+#   names.collect { |name| puts badge_maker(name) }
+# ### for each speaker in array implemanet badge_maker
+# end
+# end
 
-def assign_rooms(speakers)
-    names.collection.each_with_index do |speaker, index|
-        "Hello, #{speaker}! You'll be assigned to room #{inde+1}!"
-    ### for each speaker assign_rooms in range 1 through 7
-    end
-  end
+# def assign_rooms(speakers)
+#     names.collection.each_with_index do |speaker, index|
+#         "Hello, #{speaker}! You'll be assigned to room #{inde+1}!"
+#     ### for each speaker assign_rooms in range 1 through 7
+#     end
+#   end
  
 
-def printer (speakers)
-    batch_badge_creator(speakers).each do |speaker| put speaker 
-    end
-  end
+# def printer (speakers)
+#     batch_badge_creator(speakers).each do |speaker| put speaker 
+#     end
+#   end
 
-assign_rooms(speakers).each do |speaker|
-    puts speaker
-  end
+# assign_rooms(speakers).each do |speaker|
+#     puts speaker
+#   end
