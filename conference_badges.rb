7bc11d3afd5aa5a7b5af1e0513fe0c31fa680b32
AJ Guiles <ashiguiles@gmail.com>
2019-02-28 03:06:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+
+speakers=["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+
+Def badge_maker(name)
+puts "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(speakers)
+  speakers.each {|i| puts badge_maker(i) }
+### for each speaker in array implemanet badge_maker
+end
+
+def assign_rooms
+  ### for each speaker assign_rooms in range 1 through 7
+  ### return "Hello, _____! You'll be assigned to room _____!
+end
+  
+ 
